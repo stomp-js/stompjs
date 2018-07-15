606d45493e36a223057634af70637adbb8250b3b
deepak <deepak@kreatio.com>
2018-07-15 23:06:16 +0530

    Added newly added classes/interfaces to index

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
-export * from './stomp-headers';
 export * from './client';
 export * from './frame';
+export * from './message';
+export * from './stomp-config';
+export * from './stomp-headers';
+export * from './stomp-subscription';
+export * from './transaction';
+export * from './types';
 export * from './versions';
 
 // Compatibility code
-export * from './compatibility/stomp';
 export * from './compatibility/compat-client';
+export * from './compatibility/stomp';
