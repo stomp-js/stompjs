9e3983a424db9346270c356d8a07b928e9740ed3
deepak <deepak@kreatio.com>
2018-07-24 23:39:29 +0530

    dots and summary reporters

--- karma.conf.js
+++ karma.conf.js
@@ -36,7 +36,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['dots', 'summary'],
 
 
     // web server port
