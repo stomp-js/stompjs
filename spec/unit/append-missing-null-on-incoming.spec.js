1cadadb988e8d9a22cf89a6ddb0c7607f0470bb6
Deepak Kumar <deepak@kreatio.com>
2019-05-02 19:00:00 +0530

    Test case for appendMissingNULLonIncoming

---
+++ spec/unit/append-missing-null-on-incoming.spec.js
@@ -0,0 +1,52 @@
+describe("appendMissingNULLonIncoming", function () {
+  let client;
+
+  beforeEach(function () {
+    client = stompClient();
+
+    // Simulate incorrect behavior in React Native (see https://github.com/stomp-js/stompjs/issues/89)
+    client.webSocketFactory = () => {
+      const wrapperWS = new WrapperWS(new WebSocket(client.brokerURL));
+      wrapperWS.ws.onmessage = (ev) => {
+        // Convert incoming data to string if not already string
+        let data = ev.data;
+        if (typeof data !== "string") {
+          data = new TextDecoder().decode(data);
+        }
+
+        // chop everything after '\0'
+        data = data.replace(/\0.*/, '');
+        wrapperWS.onmessage(Object.assign({}, ev.data, {data: data}));
+      };
+      return wrapperWS;
+    };
+  });
+
+  afterEach(function () {
+    disconnectStomp(client);
+  });
+
+  // Find length -
+  const length = (data) => {
+    return typeof(data) === "string" ? data.length : data.byteLength;
+  };
+
+  it("Should append missing null in incoming frames (bypass bug in React Native)", function (done) {
+
+    client.appendMissingNULLonIncoming = true;
+
+    const body = randomText();
+    client.onConnect = function () {
+      client.subscribe(TEST.destination, function (message) {
+        expect(message.body).toEqual(body);
+        client.deactivate();
+
+        done();
+      });
+
+      client.publish({destination: TEST.destination, body: body});
+    };
+    client.activate();
+  });
+
+});
