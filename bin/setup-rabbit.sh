a7df49303876cd05b1a8f4424e7e692972556790
Deepak Kumar <deepak@kreatio.com>
2020-09-23 09:12:20 +0530

    Refactor Travis - move RabbitMQ setup to its own script

---
+++ bin/setup-rabbit.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -ex
+
+docker build -t rabbitmq:official-alpine-with-webstomp rabbitmq/
+docker run -d --hostname rabbitmq --name rabbitmq -p 15674:15674 rabbitmq:official-alpine-with-webstomp
+
+docker exec rabbitmq rabbitmqctl status
