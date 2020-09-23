e6ecb48a5f5f73eccb2f447b10dd9869a73564e1
Deepak Kumar <deepak@kreatio.com>
2020-09-23 11:19:52 +0530

    Use RabbitMQ 3.8.8 for tests

--- bin/setup-rabbit.sh
+++ bin/setup-rabbit.sh
@@ -4,5 +4,3 @@ set -ex
 
 docker build -t rabbitmq:official-alpine-with-webstomp rabbitmq/
 docker run -d --hostname rabbitmq --name rabbitmq -p 15674:15674 rabbitmq:official-alpine-with-webstomp
-
-docker exec rabbitmq rabbitmqctl status
