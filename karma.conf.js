a06a7a37bfa60a4c8e4affcfab48aff25b3b45b3
Deepak Kumar <deepak@kreatio.com>
2020-10-05 17:33:31 +0530

    Remove 'dots' reporter, causes log output to be duplicated.

--- karma.conf.js
+++ karma.conf.js
@@ -37,7 +37,7 @@ module.exports = function (config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['dots', 'summary'],
+    reporters: ['summary'],
 
     // web server port
     port: 9876,
