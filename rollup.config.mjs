dc452ea6b4604720750759e1b9bd83123e1e6cb2
Deepak Kumar <deepak@kreatio.com>
2025-03-08 21:09:11 -0500

    Update all NPM packages. Needed to update rollup.config.mjs for the @rollup/plugin-typescript@12

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -18,7 +18,10 @@ const umdMinConf = {
 export default [
   {
     input: 'src/index.ts',
-    plugins: [typescript()],
+    // @rollup/plugin-typescript@12 required the file output to be within the outDir
+    // Accordingly after the build the npm build steps removes the *.d.ts as these are not needed
+    // for the UJS bundle
+    plugins: [typescript({compilerOptions: {outDir: 'bundles'}})],
     output: [umdConf, umdMinConf],
   },
 ];
